4fe11026061248bbc3958a5a3112d74a80b2c50e
dyslove123 <7675343.die@163.com>
2015-09-14 12:24:02 +0800

    final path
    circle

--- basic_draw_class/basic_draw.py
+++ basic_draw_class/basic_draw.py
@@ -1,7 +1,106 @@
 # -*- encoding=utf-8 -*-
 import turtle
-import sympy
+import math
 
 
+class Point(object):
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
 
+    def __add__(self, other):
+        return Point(self.x+other.x, self.y+other.y)
+
+    def __mul__(self, other):
+        return Point(self.x*other, self.y*other)
+
+    def __sub__(self, other):
+        return Point(self.x - other.x, self.y - other.y)
+
+    def __div__(self, other):
+        return Point(self.x*1.0/other, self.y*1.0/other)
+
+    def distance(self, other):
+        return math.sqrt((self.x - other.x)**2 + (self.y-other.y)**2)
+
+    def dot(self, other):
+        return self.x*other.x+self.y*other.y
+
+    def __str__(self):
+        return str((self.x, self.y))
+
+    def __eq__(self, other):
+        if abs(self.x - other.x) < 10**-6 and  abs(self.y - other.y)< 10**-6:
+            return True
+        else:
+            return False
+
+
+class Segment(object):
+    def __init__(self, p1, p2):
+        self.start = p1
+        self.end = p2
+        self.length = self.start.distance(self.end)
+
+    def __str__(self):
+        return 'start ', self.start,' end ',self.end
+
+    def distance(self, point):
+        if isinstance(point, Point):
+            seg_vector = self.end - self.start
+            pt_vector = point - self.start
+            t = seg_vector.dot(pt_vector)/self.length**2
+            if t >= 1:
+                distance = Point.distance(self.end, point)
+            elif t <= 0:
+                distance = Point.distance(self.start, point)
+            else:
+                distance = Point.distance(
+                    self.start + Point(t*seg_vector.x, t*seg_vector.y), point)
+            return distance
+
+
+class Circle(Point):
+    def __init__(self, point, r):
+        super(Circle, self).__init__(point.x, point.y)
+        self.p = point
+        self.r = r
+
+    def __eq__(self, other):
+        if abs(self.r - other.r) < 10**-6 and self.p == other.p:
+            return True
+        else:
+            return False
+
+    def contain(self, point):
+        if self.p.distance(point) <= self.r:
+            return True
+        else:
+            return False
+
+    def inter(self, other):
+        if self.p.distance(other.p) < self.r+other.r:
+            return True
+        else:
+            return False
+
+class Polygon(object):
+
+    def __init__(self, *argv):
+        self.vertices = list(argv)
+        self.sides = self.sides()
+
+    def __str__(self):
+        return self.vertices
+
+    def translate(self, x, y):
+        tran = Point(x, y)
+        return Polygon(*[point + tran for point in self.vertices])
+
+    def sides(self):
+        res = []
+        args = self.vertices
+        for i in xrange(-len(args), 0):
+            res.append(Segment(args[i], args[i + 1]))
+        return res
 
--- draw.py
+++ draw.py
@@ -1,9 +1,9 @@
 # -*- encoding=utf-8 -*-
 import turtle
-from sympy import *
+from basic_draw_class.basic_draw import *
 import time
 from math import sqrt
-from sympy.core.numbers import Float as symFloat
+
 P0 = 40
 RADIUS = 10
 Kr = 1
@@ -26,8 +26,7 @@ def test_draw():
 def test_calculate_f1():
     line = [Point(-50, -50), Point(-50, 0), Point(50, 40)]
     draw(line)
-    l1 = Line(line[0], line[1])
-    l2 = Line(line[1], line[2])
+
     f1 = calculate_f1(*line)
     print f1
     draw([line[1], line[1] + f1*100])
@@ -69,7 +68,6 @@ def calculate_f2(point, obstacle_tuple):
                     calculate_bubble_max_size_in_p(Point(point.x, point.y + h*y), obstacle_tuple)
         pian_dao = Point(new_x, new_y) / (2.0 * h)
         Fr = pian_dao * Kr * (p-P0)
-        Fr = Fr.evalf()
         print Fr
         return Fr
 
@@ -79,14 +77,12 @@ def calculate_bubble_max_size_in_p(point, obstacle_tuple):
     for obstacle in obstacle_tuple:
         for side in obstacle.sides:
             t_dis = side.distance(point)
-            t_dis_list.append(t_dis.evalf())
+            t_dis_list.append(t_dis)
     return min(t_dis_list)
 
 
 def unit_length(start, direction):
-    t_length = Point.distance(start, direction)
-    t_length = t_length.evalf()
-    return Point((direction.x-start.x)*1.0/t_length, (direction.y-start.y)*1.0/t_length)
+    return Point((direction.x-start.x), (direction.y-start.y))/start.distance(direction)
 
 
 def calculate_f1(start_p, mid_p, end_p):
@@ -103,29 +99,88 @@ def draw(point_tuple, close=False):
     pen.hideturtle()
     pen.up()
     pen.setpos(start.x*basic_mul, start.y*basic_mul)
-
     pen.down()
-    pen.speed(5)
     for p in point_tuple:
+
         pen.setpos(p.x*basic_mul, p.y*basic_mul)
         pen.dot(10, 'red')
+        if isinstance(p, Circle):
+            pen.speed(0)
+            pen.up()
+            pen.forward(p.r*basic_mul)
+            pen.left(90)
+            pen.down()
+            pen.circle(p.r*basic_mul)
+            pen.up()
+            pen.setpos(p.x*basic_mul, p.y*basic_mul)
+            pen.down()
+
     if close is True:
         pen.setpos(start.x*basic_mul, start.y*basic_mul)
     return pen
 
 
-def divide_line_into_mul_point(start, end, length=20):
-    t_length = start.distance(end).evalf()
-    num_p = t_length / length + 0.5
-    direction = ((end - start) / num_p).evalf()
+def divide_line_into_mul_circle(start, end, obstacle_list):
+    # input two point and return a list of circle
+
+    t_length = start.distance(end)
+    direction = ((end - start) / end.distance(start))
+    m = 0.0
     result_list = list()
-    for i in range(num_p):
-        result_list.append(start + direction * i)
-    result_list.append(end)
+    start_p = start
+    while m < t_length:
+        r = calculate_bubble_max_size_in_p(start_p, obstacle_list)
+        result_list.append(Circle(start_p, r))
+        m += r
+        start_p = start + direction * m
+    r = calculate_bubble_max_size_in_p(end, obstacle_list)
+    result_list.append(Circle(end, r))
     return result_list
 
 
-def main():
+def remove_too_close_point_in_path(path):
+    i = 0
+    while i < len(path) - 2:
+        if path[i].distance(path[i+2]) < 0.8*(path[i].r+path[i+2].r):
+            del path[i+1]
+        else:
+            i += 1
+    return path
+
+
+def add_point_to_path(path, obstacle_list):
+    new_path = list()
+    for i in range(len(path)-1):
+        distance = path[i].distance(path[i+1])
+        if distance < path[i].r+path[i+1].r:
+            new_path.append(path[i])
+            continue
+        else:
+            new_path.append(path[i])
+            p = path[i].p + (path[i+1].p-path[i].p)*path[i].r/distance
+            new_path.append(Circle(p, calculate_bubble_max_size_in_p(p, obstacle_list)))
+    new_path.append(path[-1])
+    return new_path
+
+
+def flush_point_in_path(path, obstacle_list):
+    new_path = list()
+    for p in path:
+        new_path.append(Circle(p, calculate_bubble_max_size_in_p(p, obstacle_list)))
+    return new_path
+
+
+def remove_duplicate_point_in_path(path):
+    i = 0
+    while i < len(path) - 1:
+        if path[i] == path[i+1]:
+            del path[i+1]
+        else:
+            i += 1
+    return path
+
+
+def final_main():
     obstacle = Polygon(Point(-50, -50), Point(-50, 0), Point(0, 20), Point(30, 0), Point(30, -50))
     obstacle_list = list()
     obstacle_list.append(obstacle)
@@ -134,23 +189,17 @@ def main():
     path = [Point(0, -80), Point(-70, -60), Point(-80, 30), Point(40, 30), Point(40, 20), Point(70, 20)]
     new_path = list()
     for i in range(len(path)-1):
-        new_path += divide_line_into_mul_point(path[i], path[i+1])
-    i = 0
-    while i < len(new_path) - 1:
-        if new_path[i] == new_path[i+1]:
-            del new_path[i+1]
-        else:
-            new_path[i] = new_path[i].evalf()
-            i += 1
+        new_path += divide_line_into_mul_circle(path[i], path[i+1], obstacle_list)
+
+    new_path = remove_duplicate_point_in_path(new_path)
+    new_path = remove_too_close_point_in_path(new_path)
 
     for ob in obstacle_list:
         draw(ob.vertices, True)
-
-
+    draw(path)
     pen1 = draw(new_path)
-    f2_list = list([Point(0, 0)])
-    f1_list = list([Point(0, 0)])
-    for _ in range(5):
+
+    for _ in range(50):
         f2_list = list([Point(0, 0)])
         f1_list = list([Point(0, 0)])
         for i in range(len(new_path)-2):
@@ -163,12 +212,19 @@ def main():
         # 优化力的计算
         f_opti = [Point(0, 0)]
         for i in range(len(f_total)-2):
-            dis = Point.distance(new_path[i], new_path[i+2]).evalf()
-            u = Point(new_path[i+2]-new_path[i]).evalf()
-            dian_cheng = f_total[i+1].x * u.x + f_total[i+1].y + u.y
+            dis = Point.distance(new_path[i], new_path[i+2])
+            u = new_path[i+2]-new_path[i]
+            dian_cheng = f_total[i+1].dot(u)
             f_opti.append(f_total[i+1] - f_total[i+1] * dian_cheng / dis / dis)
         f_opti.append(Point(0, 0))
-        new_path = [new_path[i]+f_opti[i] for i in range(len(f1_list))]
+
+        new_path = [Circle(new_path[i]+f_opti[i], 0) for i in range(len(f1_list))]
+        new_path = flush_point_in_path(new_path, obstacle_list)
+        new_path = remove_duplicate_point_in_path(new_path)
+        new_path = remove_too_close_point_in_path(new_path)
+        new_path = add_point_to_path(new_path, obstacle_list)
+
+
         pen2 = draw(new_path)
         pen1.clear()
         pen1 = pen2
@@ -176,7 +232,7 @@ def main():
 
 
 if __name__ == '__main__':
-    main()
+    final_main()
 
 
 
